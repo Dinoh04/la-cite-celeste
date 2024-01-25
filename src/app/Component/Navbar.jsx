1ef18e07d91b31843f797cd614c8b1eb4796c5ee
Dinoh <dino.rainimarolahy@gmail.com>
2024-01-25 18:04:01 +0100

    Feat: create navbar

--- src/app/Component/Navbar.jsx
+++ src/app/Component/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
 
 export default function Navbar() {
   return (
-    <div><p>Heyyyyyyyyyyyyyyyyyyy</p></div>
+    <nav>
+        <div className="logo">
+            <img className='logo-img' src="/Img/logo.png" alt=""/>
+        </div>
+        <div className="nav-links">
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Nos réunions</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Témoignage</a></li>
+        </div>
+
+        <div className="social-contain">
+             <div className="Fb"><FaFacebookF /></div>
+             <div className="Instagram"><FaInstagram /></div>
+             <div className="Whatsapp"><FaWhatsapp /></div>
+        </div>
+    </nav>
   )
 }
