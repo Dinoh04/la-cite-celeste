7e08002d95bed679ec8b00ccd044e1fcde09a1b6
Dinoh <dino.rainimarolahy@gmail.com>
2024-03-06 13:10:13 +0100

    Feat: create about section

--- src/app/Component/Navbar.jsx
+++ src/app/Component/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
 
 export default function Navbar() {
   return (
@@ -11,16 +12,26 @@ export default function Navbar() {
         </div>
         <div className="nav-links">
           <li><a href="#">Accueil</a></li>
+          <li><a href="#">A propos</a></li>
           <li><a href="#">Nos réunions</a></li>
           <li><a href="#">Contact</a></li>
           <li><a href="#">Témoignage</a></li>
         </div>
 
         <div className="social-contain">
-             <div className="Fb"><FaFacebookF /></div>
-             <div className="Instagram"><FaInstagram /></div>
-             <div className="Whatsapp"><FaWhatsapp /></div>
+             <div className="social-icon"><FaFacebookF /></div>
+             <div className="social-icon"><FaInstagram /></div>
+             <div className="social-icon"><FaWhatsapp /></div>
         </div>
+
+                 
+          <div className="donation">
+          <button className='donation-button'>
+          Faire un don <FaRegHeart className='heart'/>
+            </button> 
+          
+          </div>
+      
     </nav>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,74 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Navbar from "./Component/Navbar"
+import Navbar from "./Component/Navbar";
+import { IoIosArrowDown } from "react-icons/io";
+import { FaArrowRight } from "react-icons/fa";
+
 
 export default function Home() {
   return (
-    <Navbar/>
+      <>
+        <section className="Home">
+        <Navbar/>
+
+        <div className="Head-container">
+          <div className="title">
+            <h1 className="head-title">La cité céleste</h1>
+          </div>
+          <div className="underline"></div>
+
+          <div className="text">
+             <p className="head-text">
+              Bienvenue sur le site officiel de la cité céleste. Chers amis, nous sommes maintenant <br />
+              des enfants de dieu, et ce que nous serons n'a pas encore été révélée, mais nous savons <br />
+              que lorsque le christ apparaitra, nous lui serons semblables, car nous le verrons tel qu'il est.
+             </p>
+          </div>
+
+          <div className="arrow">
+          <IoIosArrowDown className="arrow-down"/>
+          </div>
+        </div>
+        </section>
+
+        <section className="about">
+          
+           <div className="box-container">
+
+            <div className="first-container">
+                  <img className="first-img" src="/Img/Bible.jpg" alt="" />
+                  <div className="title-container">
+                    <h1 className="title">About our church</h1>
+                    <div className="description">
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni a provident fuga beatae qui perspiciatis id nemo sapiente expedita rerum necessitatibus voluptatem praesentium nostrum quasi unde, asperiores saepe ad sed!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam provident earum quae rem praesentium. Debitis sed ex perferendis cumque ipsa cum. Doloribus officia sequi qui? Cupiditate delectus similique veniam odit?
+                        
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="links">Nous visitez <FaArrowRight /></div>
+            </div>
+
+              <div className="second-container">
+              <div className="second-title">
+                    <h1 className="title-second">About our church</h1>
+                    <div className="description-second">
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni a provident fuga beatae qui perspiciatis id nemo sapiente expedita rerum necessitatibus voluptatem praesentium nostrum quasi unde, asperiores saepe ad sed!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam provident earum quae rem praesentium. Debitis sed ex perferendis cumque ipsa cum. Doloribus officia sequi qui? Cupiditate delectus similique veniam odit?
+                      </p>
+                      <div className="links-second">Nous rejoindre <FaArrowRight /></div>
+              </div>
+              <div className="img-container"><img className="second-img" src="/Img/Friends.jpg" alt="" /></div>
+               
+           </div>
+           
+           </div>
+           </div>
+        </section>
+      
+     </>  
+
+    
   );
 }
